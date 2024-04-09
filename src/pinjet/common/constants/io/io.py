72b50e1dabd8a11e232218271ea65681c8375a11
Abrar <abetrack3@yahoo.com>
2024-04-09 17:00:38 +0600

    added: python script extension and list of skippable directories

--- src/pinjet/common/constants/io/io.py
+++ src/pinjet/common/constants/io/io.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from strenum import StrEnum
 
 
@@ -10,3 +12,22 @@ class FileIOMode(StrEnum):
 
 ILLEGAL_DIRECTORY_CHARACTERS = r'[:*?<>"\'|]'
 ILLEGAL_FILENAME_CHARACTERS = r'[/]'  # More characters will be added in future on case by case basis
+
+PYTHON_SCRIPT_EXTENSION: str = '.py'
+
+SKIPPABLE_DIRECTORIES: List[str] = [
+    'build',
+    'bin',
+    'lib',
+    'Lib',
+    'dist',
+    'lib64',
+    'share',
+    'MACOSX',
+    'Scripts',
+    'include',
+    'Include',
+    '.egg-info',
+    'pyvenv.cfg',
+    '__pycache__',
+]
